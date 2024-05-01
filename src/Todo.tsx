79030e3ff0c3678e7854fea51bb9eae70bfc9b05
tomurayuri <yuri.tomura@nijibox.co.jp>
2024-05-01 23:39:20 +0900

    stateを使用したマークアップ

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 import "./Todo.css"
 
 export const Todo = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["todo1","todo2"]);
+  const [completeTodos, setCompleteTodos] = useState(["todo1でしたあ","todo2でしたあ"]);
+
   return (
     <>
       <div className="input-area"> 
@@ -10,67 +14,25 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p className="todo-item">TODO1</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
-          <li className="list-row">
-            <p className="todo-item">TODO2</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {incompleteTodos.map((todo) => (
+            <li key={todo} className="list-row">
+              <p className="todo-item">{todo}</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+            ))}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p className="todo-item">TODO1でした</p>
-            <button>戻す</button>
-          </li>
-          <li className="list-row">
-            <p className="todo-item">TODO2でした</p>
-            <button>戻す</button>
-          </li>
+          {completeTodos.map((todo) => (
+            <li key={todo} className="list-row">
+              <p className="todo-item">{todo}</p>
+              <button>戻す</button>
+            </li>
+            ))} 
         </ul>
-
       </div>
     </>)
 }
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <p>Vite + React</p>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
